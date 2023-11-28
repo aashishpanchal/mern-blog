3bff3966af2adedcf584d23f9f5720c7f5f9e6ad
Aashish Panchal <aipanchal51@gmail.com>
2023-11-29 01:29:37 +0530

    change index

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -10,6 +10,9 @@ export default defineConfig({
       "@": path.resolve(__dirname, "src"),
     },
   },
+  build: {
+    outDir: "../server/public",
+  },
   server: {
     proxy: {
       "/api/v1": {
